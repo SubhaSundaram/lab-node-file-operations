b71a2da8aca0faedf83d9038d7304a5ebc3aa8ea
selvakumar01k-51519 <selvakumar01.k@matrimony.com>
2021-10-21 19:51:42 +0530

    first commit

--- data.js
+++ data.js
@@ -1,1 +1,9 @@
-
+data=(name,year,qualification)=>{
+    data={
+        name:name,
+        Year:year,
+        Qualification:qualification
+    }
+    return ("UserId ->"+data.name+",\n"+"Name ->"+data.Year+",\n"+"Qualification ->"+data.Qualification+",\n")
+}
+module.exports.data
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+require('./data')
+const os = require('os')
+const fs=require('fs')
+const crypto=require('crypto')
+var userdata= data("Prograd",2020,"BE")
+console.log(userdata)
+let userinf=os.userInfo().username;
+const password = crypto.createHmac('sha256', 'password').update('Prograd').digest('hex');
 
+fs.writeFile('message.txt',"ProGrad Details\n"+userdata,function(err){
+    if(err)
+    throw err
+    })
+
+fs.appendFile('message.txt',"Username ->"+userinf+"\n",function(err){
+    if(err)
+    throw err
+})
+fs.appendFile('message.txt',"Password ->"+password+"\n",function(err){
+    if(err)
+    throw err
+})
